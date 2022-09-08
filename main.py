47c97be3845d6c40b98c749a369fa5ae425f1e72
Jonas <jonas.magnus@rwu.de>
2022-09-08 19:32:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Umschalt+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# ToDo
+# 1 read and save layers cfg
+# 2 Read in tile images
+# 3 Read mask
+# 4 refer mask color to tile image by layers cfg
+# 5 create Satmap
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
