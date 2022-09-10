8201a44bf9d8e23fb4ab4da321a05d45c20150f2
Raphael <40037381+rk-exxec@users.noreply.github.com>
2022-09-10 15:31:46 +0200

    lut implementation

--- maskToSatMap.py
+++ maskToSatMap.py
@@ -24,6 +24,8 @@ from dataclasses import dataclass
 import time
 import logging
 
+import numba as nb
+
 from pathlib import Path
 from typing import Dict
 import numpy as np
@@ -37,6 +39,18 @@ class Surface:
     name: str = ""
     path: str = "path"
     mask_color: tuple[int, int, int] = (255, 255, 255)
+    avg_color: tuple[int, int, int] = (255, 0, 255)
+
+    @staticmethod
+    def get_mask_avg_col_map(surfaces: list[Surface]):
+        map = {}
+        nmap={}
+        for surf in surfaces:
+            map[surf.mask_color] = surf.avg_color
+            nmap[surf.mask_color] = surf.name
+        return map, nmap
+
+        
 
 def read_layers_cfg(path):
     """ Reads a arma3 layers.cfg and graps mask color to suface material information
@@ -69,30 +83,59 @@ def read_layers_cfg(path):
     logging.debug (surfaces)
     return surfaces
 
+def unique_scikit(ar):
+    if ar.ndim != 2:
+        raise ValueError("unique_rows() only makes sense for 2D arrays, "
+                         "got %dd" % ar.ndim)
+    # the view in the next line only works if the array is C-contiguous
+    ar = np.ascontiguousarray(ar)
+    # np.unique() finds identical items in a raveled array. To make it
+    # see each row as a single item, we create a view of each row as a
+    # byte string of length itemsize times number of columns in `ar`
+    ar_row_view = ar.view('|S%d' % (ar.itemsize * ar.shape[1]))
+    _, unique_row_indices, idx = np.unique(ar_row_view, return_index=True, return_inverse=True)
+    ar_out = ar[unique_row_indices]
+    return ar_out, idx
+
 def replace_mask_color(mask_path, surfaces: Dict[str, Surface], target_path):
     """replaces the mask colors with the average colors of the corresponding texture as defined in layers.cfg"""
     img = Image.open(mask_path).convert('RGB')
     logging.info(f"Mask loaded {img.size}px")
     mask = np.array(img)
-    mask_size = int(mask.size/3)
 
-    replace_counter = 0
+    # create lookup table and corresponding index array from input image
+    logging.info("Preprocessing mask image, this might take a while ...")
+    lut, idx_arr = np.unique(mask.reshape(-1, mask.shape[2]), return_inverse=True, axis=0)
+    idx_arr = idx_arr.reshape(mask.shape[0:2])
 
-    for surf in surfaces.values():
-        # calculate average color from texture stored in surface
-        paa_path = find_paa_path(surf.path)
-        avg_c = get_paa_avg_col(paa_path)
-        # replace color from surface with average color in mask
-        # ToDO Add a counter so we can see how many px where replaced ! Must be in sum the same as the mask size !
-        replace_px = (mask == surf.mask_color).all(axis = -1)
-        mask[replace_px] = avg_c
-        cnt = np.count_nonzero(replace_px)
-        replace_counter += cnt
-        print(f"replaced {cnt} pixels for material {surf.name}: {surf.mask_color}\tto average ground texture color {avg_c}\t\ttotal replaced pixels {replace_counter:,d}/{mask_size:,d}\t {replace_counter/mask_size * 100:3.1f}%")
-        
+    # get color map from loaded layers.cfg and contained textures average colors
+    logging.info("Building colormap from textures")
+    color_map, name_map = Surface.get_mask_avg_col_map(surfaces.values())
 
-    print(f"Exporting sat map to {target_path}")
-    out = Image.fromarray(mask)
+    # build new lookup table with average texture colors
+    logging.info("Building new lookup table")
+    new_lut = np.zeros_like(lut)
+    for i, col in enumerate(lut):
+        col = tuple(col)
+        try:
+            new_lut[i] = color_map[col]
+            # check for unused colors in mask by comparing with list from layers.cfg
+            # this removes all colors in mask from this color map, if not empty by end, it contains unused textures/colors
+            name_map.pop(col)
+        except KeyError:
+            logging.warning(f"Mask color {col} not in lookup table! Not replacing. If you think this is an error, check your layers.cfg and materials.")
+            new_lut[i] = col
+
+    # check unused textures
+    if name_map:
+        logging.warning(f"Unused textures:\n {name_map.values()}")
+
+    # apply new lookup table to index array to get new sat image
+    logging.info("Applying new lookup table")
+    sat_map = new_lut[idx_arr]
+
+    logging.info(f"Exporting sat map to {target_path}")
+    out = Image.fromarray(sat_map)
     out.save(target_path)
 
 
@@ -109,16 +152,24 @@ def get_paa_avg_col(path):
     param path: the path to the paa file
     returns:
     """
-    path = Path(path)
-    data = np.memmap(path, dtype=np.uint8)
-    if not(data[1] == 0xFF and data[0] == 0x01): return (0, 0, 0) # dxt1 file?
+    # path = Path(find_paa_path(path))
+    # data = np.memmap(path, dtype=np.uint8)
+    # if not(data[1] == 0xFF and data[0] == 0x01): return (0, 0, 0) # dxt1 file?
 
-    avg = data[0x0e:0x12] # bgra
-    avg_r = avg[2]
-    avg_g = avg[1]
-    avg_b = avg[0]
-    avg_a = avg[3]
+    # avg = data[0x0e:0x12] # bgra
+    # avg_r = avg[2]
+    # avg_g = avg[1]
+    # avg_b = avg[0]
+    # avg_a = avg[3]
+    avg_r, avg_g, avg_b = random.randbytes(3)
     return (avg_r, avg_g, avg_b) #, avg_a)
+
+def load_average_colors(surfaces: dict[str, Surface]):
+    for surf in surfaces.values():
+        # calculate average color from texture stored in surface
+        surf.avg_color = get_paa_avg_col(surf.path)
+
+    return surfaces
 
 
 def start(layers, mask, output):
@@ -136,28 +187,27 @@ def start(layers, mask, output):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-    print(sys.argv)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("layers", type=str, help="path of the layers.cfg file")
-    parser.add_argument("mask", type=str, help="the terrain mask .tiff image file")
-    parser.add_argument("-wd", "--workdrive", type=str, default="P:\\", help="drive letter of the Arma3 tools work drive")
-    parser.add_argument("-o", "--output", type=str, default="./sat_img.tiff", help="path of the resulting sat view image file")
-    parser.add_argument("-D","--Debug", action="store_true", help="increases verbosity")
-    # parser.add_argument("-cwd, --working_directory", help="working directory of this python file")
-    # args = parser.parse_args(sys.argv)
-    args = parser.parse_args()
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     sh = logging.StreamHandler(sys.stdout)
     sh.setLevel(logging.DEBUG)
     logger.addHandler(sh)
+    # print(sys.argv)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("layers", type=str, help="path of the layers.cfg file")
+    # parser.add_argument("mask", type=str, help="the terrain mask .tiff image file")
+    # parser.add_argument("-wd", "--workdrive", type=str, default="P:\\", help="drive letter of the Arma3 tools work drive")
+    # parser.add_argument("-o", "--output", type=str, default="./sat_img.tiff", help="path of the resulting sat view image file")
+    # parser.add_argument("-D","--Debug", action="store_true", help="increases verbosity")
+    # # parser.add_argument("-cwd, --working_directory", help="working directory of this python file")
+    # # args = parser.parse_args(sys.argv)
+    # args = parser.parse_args()
     
-    assert os.path.exists(args.layers), f"Layers file {args.layers} does not exist"
-    assert os.path.exists(args.mask),   f"Mask file {args.mask} does not exist"
+    # assert os.path.exists(args.layers), f"Layers file {args.layers} does not exist"
+    # assert os.path.exists(args.mask),   f"Mask file {args.mask} does not exist"
     
-    if (args.output != "./sat_img.tiff"):
-        assert os.path.exists(args.output),  f"Output directory {args.output} does not exist"
+    # if (args.output != "./sat_img.tiff"):
+    #     assert os.path.exists(args.output),  f"Output directory {args.output} does not exist"
     
 
     # if args.Debug: 
@@ -171,5 +221,6 @@ if __name__ == '__main__':
     #         drv += "\\"
     #     WORKDRIVE = drv
 
-    start(args.layers,args.mask,args.output)
+    # start(args.layers,args.mask,args.output)
+    start("a3_SourceData/layers.cfg","a3_SourceData/mask_underground.tiff", "a3_SourceData/sat_map.tiff")
     
