6f223e0becf47585177e64ab3d939f0634efa798
Raphael <40037381+rk-exxec@users.noreply.github.com>
2022-09-10 22:05:57 +0200

    error handling

--- maskToSatMap.py
+++ maskToSatMap.py
@@ -31,12 +31,15 @@ from PIL import Image
 
 WORKDRIVE = "P:\\"
 
+ERRORCOLOR = (255,0,255)
+ERRORCOLOR_32 = 0xFF00FF
+
 @dataclass
 class Surface:
     name: str = ""
-    path: str = "path"
+    path: str = ""
     mask_color: int = 0xFFFFFF
-    avg_color: tuple[int,int,int] = (255, 0, 255)
+    avg_color: tuple[int,int,int] = ERRORCOLOR
 
 
 def get_mask_avg_col_map(surfaces: list[Surface]):
@@ -45,7 +48,7 @@ def get_mask_avg_col_map(surfaces: list[Surface]):
     param surfaces: list of Surface objects
     returns: array(256^3,3) colormap, dict[int,str] mask color to texture name map
     """
-    col_map = np.full((256**3,3), (255,0,255), dtype=np.uint8)
+    col_map = np.full((256**3,3), ERRORCOLOR, dtype=np.uint8)
     nmap={}
     for surf in surfaces:
         col_map[surf.mask_color] = surf.avg_color
@@ -59,27 +62,40 @@ def read_layers_cfg(path):
     param path: the path to the layers.cfg file
     returns: dict of [texturename, Surface]
     """
-    logging.info("Reading Layers.cfg")
     layers_cfg = open(path, "r")
-    all = "".join(layers_cfg.readlines())
+    file_contents = "".join(layers_cfg.readlines())
     surfaces = {}
     
     pattern_name_rgb = r"\s+(\w+)\[\s*\]\s*=\s*{\s*{\s*(\s*\d{1,3}\s*),(\s*\d{1,3}\s*),(\s*\d{1,3}\s*)"
 
-    matches = re.finditer(pattern_name_rgb, all)
-
+    matches = re.finditer(pattern_name_rgb, file_contents)
+    mask_colors = []
     for match in matches:
         r, g, b = int(match.group(2)), int(match.group(3)), int(match.group(4))
         # load color as 32 bit int for performance reasons
-        surfaces[match.group(1)] = Surface(name=match.group(1), path="", mask_color=((r << 16) + (g << 8) + b))
+        color_32 = ((r << 16) + (g << 8) + b)
+        name = match.group(1)
+
+        # check if color already loaded
+        if color_32 in mask_colors:
+            logging.error(f"Duplicate mask color entry in layers.cfg: {name} - {(r,g,b)}. Entry ignored!")
+            continue
+
+        if name in surfaces.keys():
+            logging.error(f"Duplicate texture entry in layers.cfg: {name}. Entry ignored!")
+            continue
+
+        mask_colors.append(color_32)        
+        surfaces[name] = Surface(name=name, path="", mask_color=color_32)
 
     for key in surfaces.keys():
         pattern_name_path = r"class\s+" + key + r"\s+{\s+.*\n\s*material\w*=\w*(.*)"
 
-        matches = re.finditer(pattern_name_path, all)
-
-        for match in matches:
-            surfaces[key].path = match.group(1).replace(";", "").replace('"', "")
+        matches = re.findall(pattern_name_path, file_contents)
+        if matches:
+            surfaces[key].path = matches[0].replace(";", "").replace('"', "")
+        else:
+            logging.error(f"No material file path found for texture entry in layers.cfg: {key}.")
 
     
     logging.debug("layers.cfg read")
@@ -107,7 +123,7 @@ def replace_mask_color(mask_path, surfaces: Dict[str, Surface], target_path):
 
     # check for missing textures, 0xFF00FF (pink) is default value of color map
     color_map_32: np.ndarray = color_map.dot(np.array([0x10000, 0x100, 0x1], dtype=np.int32))
-    error_pixels_cnt = np.count_nonzero(color_map_32[mask_32] == 0xFF00FF)
+    error_pixels_cnt = np.count_nonzero(color_map_32[mask_32] == ERRORCOLOR_32)
     if error_pixels_cnt:
         logging.warning(f"There are missing textures. Areas will show as pink on sat map. Total pixel errors: {error_pixels_cnt}")
 
@@ -127,31 +143,43 @@ def replace_mask_color(mask_path, surfaces: Dict[str, Surface], target_path):
 
 def find_paa_path(rvmat_path):
     """Extracts the path of the paa file corresponding to the given rvmat file"""
-    with open(WORKDRIVE + rvmat_path) as file:
-        for line in file:
-            if "_CO.paa" in line:
-                return WORKDRIVE + line.split("=")[1].replace(";", "").replace('"', "").strip()
+    try:
+        with open(WORKDRIVE + rvmat_path) as file:
+            for line in file:
+                if "_CO.paa" in line:
+                    return WORKDRIVE + line.split("=")[1].replace(";", "").replace('"', "").strip()
+    except:
+        return None
 
 def get_paa_avg_col(path):
-    """ Reads a arma3 paa file and returns the average color as tuple RGB
+    """ Reads a arma3 rvmat file and returns the average color of the corresponding paa file as tuple RGB
 
     param path: the path to the paa file
-    returns:
+    returns: the average color as defined in the paa file as (R,G,B) tuple
     """
-    path = Path(find_paa_path(path))
-    data = np.memmap(path, dtype=np.uint8)
-    if not(data[1] == 0xFF and data[0] == 0x01): return (0, 0, 0) # dxt1 file?
 
-    avg = data[0x0e:0x12] # bgra
-    avg_r = avg[2]
-    avg_g = avg[1]
-    avg_b = avg[0]
-    # avg_a = avg[3]
-    # avg_r, avg_g, avg_b = random.randbytes(3)
+    paa_path = find_paa_path(path)
+    if not paa_path: 
+        logging.error(f"Cannot extract paa path from rvmat file {path}")
+        return ERRORCOLOR
+
+    try:
+        data = np.memmap(paa_path, dtype=np.uint8)
+    except FileNotFoundError:
+        logging.error(f"Cannot open paa file {paa_path}")
+        return ERRORCOLOR
+
+    if not(data[1] == 0xFF and data[0] == 0x01): return ERRORCOLOR # dxt1 file?
+
+    avg_b, avg_g, avg_r  = data[0x0e:0x11] # bgr
+    avg_r, avg_g, avg_b = random.randbytes(3)
     return avg_r, avg_g, avg_b
 
 def load_average_colors(surfaces: dict[str, Surface]):
     for surf in surfaces.values():
+        if not surf.path:
+            logging.error(f"Texture {surf.name} has no material path.")
+            continue
         # calculate average color from texture stored in surface
         surf.avg_color = get_paa_avg_col(surf.path)
     return surfaces
@@ -173,6 +201,7 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
     sh.setLevel(logging.DEBUG)
     logger.addHandler(sh)
     # print(sys.argv)
@@ -183,8 +212,9 @@ if __name__ == '__main__':
     parser.add_argument("-o", "--output", type=str, default="./sat_img.tiff", help="path of the resulting sat view image file")
     parser.add_argument("-D","--Debug", action="store_true", help="increases verbosity")
     # parser.add_argument("-cwd, --working_directory", help="working directory of this python file")
-    # args = parser.parse_args(sys.argv)
-    args = parser.parse_args()
+
+    args = parser.parse_args(sys.argv)
+    # args = parser.parse_args(["maskToSatMap.py", "a3_SourceData/layers.cfg","a3_SourceData/mask_underground.tiff", "-o", "a3_SourceData/sat_map.tiff"])
     
     assert os.path.exists(args.layers), f"Layers file {args.layers} does not exist"
     assert os.path.exists(args.mask),   f"Mask file {args.mask} does not exist"
@@ -205,5 +235,4 @@ if __name__ == '__main__':
     #     WORKDRIVE = drv
 
     start(args.layers, args.mask, args.output)
-    # start("a3_SourceData/layers.cfg","a3_SourceData/mask_underground.tiff", "a3_SourceData/sat_map.tiff")
     
